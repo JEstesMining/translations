34d27aa0a31824ceef50caa5456126324034fd84
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-05-22 17:29:58 -0400

    updates

--- scrips/init.sh
+++ scrips/init.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env sh
 set -e
 
+# Update Packages
 apt update
+
+# Install deps
 apt install -y php-cli
+
+# Install composer into /app/bin
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --install-dir=/app/bin --filename=composer
+rm composer-setup.php
