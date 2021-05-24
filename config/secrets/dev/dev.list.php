ce32325b5eb341062ea76f681775d99a1378b276
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-05-24 09:57:32 -0400

    trying this secrets shit out

---
+++ config/secrets/dev/dev.list.php
@@ -0,0 +1,10 @@
+<?php
+
+return array (
+  'APP_SECRET' => NULL,
+  'AWS_ACCESS_KEY_ID' => NULL,
+  'AWS_SECRET_ACCESS_KEY' => NULL,
+  'DATABASE_URL' => NULL,
+  'LOCK_DSN' => NULL,
+  'REDIS_URL' => NULL,
+);
