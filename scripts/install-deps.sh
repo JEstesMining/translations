ae661b3c41ad40995d7df9ef05bf1a0f2399e12a
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-05-25 19:55:54 -0400

    moot

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -13,6 +13,6 @@ chown -R www-data:www-data /app/vendor
 
 
 #DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
-yarn install --cwd /app --cache-folder /tmp/yarn --non-interactive --prod
-yarn run --cwd /app --cache-folder /tmp/yarn --non-interactive --prod production
+yarn --cwd /app --cache-folder /tmp/yarn --non-interactive --prod install
+yarn --cwd /app --cache-folder /tmp/yarn --non-interactive --prod run build
 chown -R www-data:www-data /app/node_modules
