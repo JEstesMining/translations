0c7c339e21d5b424ca5dfe355f01125dea8186ae
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-05-25 19:40:39 -0400

    yarn

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -6,10 +6,12 @@ export COMPOSER_ALLOW_SUPERUSER=1
 export COMPOSER_HOME=/tmp/composer
 export COMPOSER_VENDOR=/app/vendor
 DEPLOYMENT_ARCHIVE=/app
-#DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
 
 composer install --working-dir=${DEPLOYMENT_ARCHIVE} --prefer-dist --no-dev --optimize-autoloader
 chown -R www-data:www-data /app/var
 chown -R www-data:www-data /app/vendor
 
-#yarn install
+
+#DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
+yarn install --cwd /app --cache-folder /tmp/yarn --non-interactive
+yarn run production
