dc93e0c7cdf8b3290ceb08ccdb4006fc201a024d
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-05-25 19:13:24 -0400

    meh

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -5,7 +5,7 @@ export APP_ENV=prod
 export COMPOSER_ALLOW_SUPERUSER=1
 export COMPOSER_HOME=/tmp/composer
 #export COMPOSER_VENDOR=/app/vendor
-DEPLOY_PATH=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}
+DEPLOY_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
 
-composer install --working-dir=/app --prefer-dist --no-dev --optimize-autoloader
+composer install --working-dir=${DEPLOYMENT_ARCHIVE} --prefer-dist --no-dev --optimize-autoloader
 #yarn install
