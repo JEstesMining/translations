a4a6395effa0160675fec6a2e92077718748fcea
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-05-25 19:30:45 -0400

    permissions

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -9,4 +9,7 @@ DEPLOYMENT_ARCHIVE=/app
 #DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
 
 composer install --working-dir=${DEPLOYMENT_ARCHIVE} --prefer-dist --no-dev --optimize-autoloader
+chown -R www-data:www-data /app/var
+chown -R www-data:www-data /app/vendor
+
 #yarn install
