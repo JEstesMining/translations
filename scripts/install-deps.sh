e98ef0c5b8c974b6240101a32de0f5f7623109ba
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-05-25 19:16:49 -0400

    typo

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -5,7 +5,7 @@ export APP_ENV=prod
 export COMPOSER_ALLOW_SUPERUSER=1
 export COMPOSER_HOME=/tmp/composer
 #export COMPOSER_VENDOR=/app/vendor
-DEPLOY_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
+DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
 
 composer install --working-dir=${DEPLOYMENT_ARCHIVE} --prefer-dist --no-dev --optimize-autoloader
 #yarn install
