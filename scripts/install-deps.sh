d46e7c789605b75f823a5539d446aee5b79b4325
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-05-25 19:21:57 -0400

    sleep

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -4,7 +4,7 @@ set -e
 export APP_ENV=prod
 export COMPOSER_ALLOW_SUPERUSER=1
 export COMPOSER_HOME=/tmp/composer
-#export COMPOSER_VENDOR=/app/vendor
+export COMPOSER_VENDOR=/app/vendor
 DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
 
 composer install --working-dir=${DEPLOYMENT_ARCHIVE} --prefer-dist --no-dev --optimize-autoloader
