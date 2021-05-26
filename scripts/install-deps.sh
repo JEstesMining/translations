a6b710b79763bd4069f290db88173a7e9ad7f476
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-05-26 18:38:26 -0400

    various updates

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env sh
 set -e
 
-mkdir /app || true
+mkdir -vp /app/{vendor,var,node_modules}
 
 export APP_ENV=prod
 export COMPOSER_ALLOW_SUPERUSER=1
 export COMPOSER_HOME=/tmp/composer
 export COMPOSER_VENDOR=/app/vendor
-DEPLOYMENT_ARCHIVE=/app
+#DEPLOYMENT_ARCHIVE=/app
+DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
 
 composer install --working-dir=${DEPLOYMENT_ARCHIVE} --prefer-dist --no-dev --optimize-autoloader
-chown -R www-data:www-data /app/var
-chown -R www-data:www-data /app/vendor
+chown -R www-data:www-data ${DEPLOYMENT_ARCHIVE}/var
+chown -R www-data:www-data ${DEPLOYMENT_ARCHIVE}/vendor
 
 
 #DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
-yarn --cwd /app --cache-folder /tmp/yarn --non-interactive --prod install
+yarn --cwd ${DEPLOYMENT_ARCHIVE} --cache-folder /tmp/yarn --non-interactive --prod install
 #yarn --cwd /app --cache-folder /tmp/yarn --non-interactive --prod run build
-chown -R www-data:www-data /app/node_modules
+chown -R www-data:www-data ${DEPLOYMENT_ARCHIVE}/node_modules
