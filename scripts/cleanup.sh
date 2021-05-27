7fdea39d38256d3cc5c8ed41dd5bf7124fe110df
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-05-26 20:00:54 -0400

    meh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,7 @@
 #aws s3 rm s3://codepipeline-us-east-1-397194491914/app.dascrypto.farm/SourceArti/** --recursive
 
 
-
+chown www-data:www-data /app
 
 service nginx restart
 service php7.4-fpm restart
