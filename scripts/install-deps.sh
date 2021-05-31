51f405276cc5c906d55fa0887a155a573e8742e0
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-05-31 19:03:16 -0400

    do this after the install

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -7,8 +7,8 @@ export APP_ENV=prod
 export COMPOSER_ALLOW_SUPERUSER=1
 export COMPOSER_HOME=/tmp/composer
 export COMPOSER_VENDOR=/app/vendor
-#DEPLOYMENT_ARCHIVE=/app
-DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
+DEPLOYMENT_ARCHIVE=/app
+#DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
 
 composer install --working-dir=${DEPLOYMENT_ARCHIVE} --prefer-dist --no-dev --optimize-autoloader --no-scripts
 chown -R www-data:www-data ${DEPLOYMENT_ARCHIVE}/var
