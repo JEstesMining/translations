d73f0f307d0800b1525a027d04a0eccf1f3d9989
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-05-31 18:57:38 -0400

    meh

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -10,7 +10,7 @@ export COMPOSER_VENDOR=/app/vendor
 #DEPLOYMENT_ARCHIVE=/app
 DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
 
-composer install --working-dir=${DEPLOYMENT_ARCHIVE} --prefer-dist --no-dev --optimize-autoloader
+composer install --working-dir=${DEPLOYMENT_ARCHIVE} --prefer-dist --no-dev --optimize-autoloader --no-scripts
 chown -R www-data:www-data ${DEPLOYMENT_ARCHIVE}/var
 chown -R www-data:www-data ${DEPLOYMENT_ARCHIVE}/vendor
 
