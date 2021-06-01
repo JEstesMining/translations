afdff5bf6b1063b216b571b97abc71bf49db8e2c
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2021-06-01 18:17:34 -0400

    clear cache on deployments

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
+# @see https://symfony.com/doc/current/deployment.html
 set -e
 
-#mkdir -vp /app
-
 export APP_ENV=prod
+export APP_DEBUG=0
 export COMPOSER_ALLOW_SUPERUSER=1
 export COMPOSER_HOME=/tmp/composer
 export COMPOSER_VENDOR=/app/vendor
@@ -14,8 +14,10 @@ composer install --working-dir=${DEPLOYMENT_ARCHIVE} --prefer-dist --no-dev --op
 chown -R www-data:www-data ${DEPLOYMENT_ARCHIVE}/var
 chown -R www-data:www-data ${DEPLOYMENT_ARCHIVE}/vendor
 
-
 #DEPLOYMENT_ARCHIVE=/opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
 yarn --cwd ${DEPLOYMENT_ARCHIVE} --cache-folder /tmp/yarn --non-interactive --prod install
 yarn --cwd ${DEPLOYMENT_ARCHIVE} --cache-folder /tmp/yarn --non-interactive --prod run build
 chown -R www-data:www-data ${DEPLOYMENT_ARCHIVE}/node_modules
+
+# Clear Symfony Cache
+APP_ENV=prod APP_DEBUG=0 php /app/bin/console cache:clear
